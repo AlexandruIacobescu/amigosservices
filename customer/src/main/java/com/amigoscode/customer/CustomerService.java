9ea37061b5a8773fa2f5bcac8a8b0015b8cf6cfc
AlexandruIacobescu <alexandru.iacobescu01@e-uvt.ro>
2024-06-22 19:25:41 +0300

    addded fraud things

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,9 +1,14 @@
 package com.amigoscode.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.security.DenyAll;
+
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
